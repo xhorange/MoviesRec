350a07431c6ca5be7df5737d836cb6ca5cb8394c
yyc <1135406917@qq.com>
2021-05-18 20:44:24 +0800

    user entity

---
+++ src/main/java/njupt/orange/movies_rec/Entity/User.java
@@ -0,0 +1,34 @@
+package njupt.orange.movies_rec.Entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class User {
+    private int userId;
+    private String userName;
+    private String passwd;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+}
